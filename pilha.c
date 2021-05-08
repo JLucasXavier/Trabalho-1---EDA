3f83fb00316aff74153c5b54a814091955cf778a
Alexandre R. Bomfim Junior <abjbomfim@outlook.com>
2021-05-08 17:15:25 -0300

    Finalização Pilha

--- pilha.c
+++ pilha.c
@@ -46,26 +46,80 @@ char pop_pilha(Pilha *p){
         exit(1);
     }
     d = p->topo;
-    printf("%c teste1\n",*d->a);
+    //printf("%c teste1\n",*d->a);
     char a=*(d->a);
-    printf("%c teste2\n",a);
+    //printf("%c teste2\n",a);
     p->topo = d->prox;
     free(d);
     return a;
 }
 
-int main(void){
-    Pilha *p = (Pilha*)malloc(sizeof(Pilha));
-    p = cria_pilha();
-    char var[] = "2+3*7";
+void monta_shuting_yard(Pilha* saida, Pilha *operador){
+	char var[] = "((a*b)-(c*d))/(e*f)";
     for (int i = 0; i < strlen(var); ++i)
     {
-        push_pilha(p, var[i]);
+    	if (var[i] == '+' || var[i] == '-' || var[i] == '*' || var[i] == '/')
+    	{
+    		if (operador->topo != NULL && ((var[i] == '+') || (var[i] == '-' && (*(operador->topo->a) == '*' || *(operador->topo->a) == '/') ) || (var[i] == '*' && *(operador->topo->a) == '/') ) )
+    		{
+    			push_pilha(saida, pop_pilha(operador));
+    		}
+    		
+    		push_pilha(operador, var[i]);
+    	}
+    	
+    	else if (var[i] == '(')
+    	{
+    		push_pilha(operador, var[i]);	
+    	}
+
+    	else if (var[i] == ')'){
+    		while(*(operador->topo->a) != '('){
+    			push_pilha(saida, pop_pilha(operador));
+    		}
+    		pop_pilha(operador);
+    	}
+
+    	else{
+    		push_pilha(saida, var[i]);
+    	}
     }
-    printf("%c\n", pop_pilha(p));
-    printf("%c\n", pop_pilha(p));
-    printf("%c\n", pop_pilha(p));
-    printf("%c\n", pop_pilha(p));
-    printf("%c\n", pop_pilha(p));
+    while(operador->topo != NULL){
+    	if (*(operador->topo->a) == '(' || *(operador->topo->a) == ')')
+    	{
+    		pop_pilha(operador);
+    	}
+    	else{
+    		push_pilha(saida, pop_pilha(operador));
+    	}
+    }
+}
+
+//void esvaziar_pilha(Pilha *num){
+//	for (int i = 0; num->topo != NULL; ++i)
+//	{
+//		printf("%c\n", pop_pilha(num));
+//	}
+//}
+
+void pilha_libera(Pilha *p){
+	Dado *d, *q = p->topo;
+	while(q != NULL){
+		d = q->prox;
+		free(q);
+		q = d;
+	}
+	free(p);
+}
+
+int main(void){
+    Pilha *num = (Pilha*)malloc(sizeof(Pilha));
+    num = cria_pilha();
+    Pilha *operador = (Pilha*)malloc(sizeof(Pilha));
+    operador = cria_pilha();
+
+    monta_shuting_yard(num, operador);
+    //esvaziar_pilha(num);
+    system("pause");
     return 0;
 }
---
+++ pilha.h
@@ -0,0 +1,13 @@
+
+
+Pilha *cria_pilha(void);
+
+void push_pilha(Pilha *p, char a);
+
+int vazia_pilha(Pilha *p);
+
+char pop_pilha(Pilha *p);
+
+void monta_shuting_yard(Pilha* saida, Pilha *operador);
+
+void pilha_libera(Pilha *p);
