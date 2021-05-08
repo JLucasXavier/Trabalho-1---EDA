299b3e01de2c7bc73b133a98bbc72dca12ff9b6d
Lucas Xavier <lucasxaviert875@gmail.com>
2021-05-08 14:35:59 -0300

    Inserção da pilha

--- pilha.c
+++ pilha.c
@@ -5,74 +5,67 @@
 typedef struct dado Dado;
 struct dado
 {
-	char a[2];
-	Dado *prox;
+    char a[2];
+    Dado *prox;
 };
 
 typedef struct pilha Pilha;
 struct pilha
 {
-	Dado *topo;
+    Dado *topo;
 };
 
 Pilha *cria_pilha(){
-	Pilha *p = (Pilha*)malloc(sizeof(Pilha));
-	if (p == NULL)
-	{
-		exit(1);
-	}
-
-	p->topo = NULL;
-	return p;
+    Pilha *p = (Pilha*)malloc(sizeof(Pilha));
+    if (p == NULL){
+        exit(1);
+    }
+    p->topo = NULL;
+    return p;
 }
 
 void push_pilha(Pilha *p, char a){
-	Dado *d = (Dado*)malloc(sizeof(Dado));
-	if(d == NULL) exit(1);
-	
-	strcpy(d->a, &a);
-	//printf("%c\n", *d->a);
-	p->topo = d;
+    Dado *d = (Dado*)malloc(sizeof(Dado));
+    if(d == NULL) exit(1);
+    strcpy(d->a, &a);
+    d->prox = p->topo;
+    p->topo = d;
 }
 
 int vazia_pilha(Pilha *p){
-	if (p->topo == NULL)
-	{
-		return 1;
-	}
-	return 0;
+    if (p->topo == NULL)
+    {
+        return 1;
+    }
+    return 0;
 }
 
-char *pop_pilha(Pilha *p){
-	Dado *d;
-	char *a;
-	if (vazia_pilha(p) == 1)
-	{
-		exit(1);
-	}
-	d = p->topo;
-	//printf("%c\n", *d->a);
-	strcpy(a, d->a);
-	p->topo = d->prox;
-	free(d);
-	return a;
+char pop_pilha(Pilha *p){
+    Dado *d = (Dado*)malloc(sizeof(Dado));
+    if (vazia_pilha(p) == 1){
+        exit(1);
+    }
+    d = p->topo;
+    printf("%c teste1\n",*d->a);
+    char a=*(d->a);
+    printf("%c teste2\n",a);
+    p->topo = d->prox;
+    free(d);
+    return a;
 }
 
-int main(void)
-{
-	Pilha *p = (Pilha*)malloc(sizeof(Pilha));
-	p = cria_pilha();
-	char var[] = "2+3*7";
-	
-	for (int i = 0; i < strlen(var); ++i)
-	{
-		//printf("%c\n", var[i]);
-		push_pilha(p, var[i]);
-	}
-	printf("%c\n", *pop_pilha(p));
-	printf("%c\n", *pop_pilha(p));
-	//printf("%s\n", pop_pilha(p));
-	//printf("%s\n", pop_pilha(p));
-	system("pause");
-	return 0;
+int main(void){
+    Pilha *p = (Pilha*)malloc(sizeof(Pilha));
+    p = cria_pilha();
+    char var[] = "2+3*7";
+    for (int i = 0; i < strlen(var); ++i)
+    {
+        push_pilha(p, var[i]);
+    }
+    printf("%c\n", pop_pilha(p));
+    printf("%c\n", pop_pilha(p));
+    printf("%c\n", pop_pilha(p));
+    printf("%c\n", pop_pilha(p));
+    printf("%c\n", pop_pilha(p));
+    return 0;
 }
